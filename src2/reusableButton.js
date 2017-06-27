5b7baeb56e8c725089dd9c487f6d2c75e5be4ed2
Rahul Singh <rahulsinghunsw@gmail.com>
2017-06-28 09:32:29 +1000

    Reusable Button

---
+++ src2/reusableButton.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+import {
+StyleSheet,
+View,
+Image,
+Text,
+ScrollView,
+TouchableHighlight,
+} from 'react-native';
+
+export default class ReusableButton extends Component {
+  // Here we are goint to use PropTypes to check the input data
+}
